7ebc6762753e9c792b4f81d08a937ce54a687a3f
Jaime E Betancourt <jaime.betancourt307@gmail.com>
2021-02-10 16:52:32 -0500

    removed contact page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import ScrollTop from "./components/ScrollTop";
 import Home from "./pages/Home";
-import Contact from "./pages/Contact";
 import Exhibits from "./pages/Exhibits";
 import Detail from "./pages/Detail";
 
